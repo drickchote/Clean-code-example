ba9a8b01d481b17539e1436d510e71577a976833
Marcos Adriano <mdrick19@gmail.com>
2023-02-12 17:52:02 -0300

    chore: fixing jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectConverageFrom: ["<rootDir>/src/**/**.ts"],
+  collectConverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transformer: {
     ".+\\.ts$": "ts-jest",
   },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
