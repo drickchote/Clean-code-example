2dd02c4263f93cc3e2a8df3474bba8a0cb5d98c9
Marcos Adriano <mdrick19@gmail.com>
2023-02-13 21:57:13 -0300

    test: add password confirmation validation in SignUpController

--- src/presentational/controllers/signup/signup.spec.ts
+++ src/presentational/controllers/signup/signup.spec.ts
@@ -49,4 +49,22 @@ describe("SignUpController", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
+
+  test("should return 400 if no password confirmation is provided ", () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        email: "any_email@email.com",
+        name: "any_name",
+        password: "any_password",
+      },
+    };
+
+    const httpResponse = sut.handler(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError("passwordConfirmation")
+    );
+  });
 });
