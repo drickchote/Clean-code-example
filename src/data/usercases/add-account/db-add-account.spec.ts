d7b45229b99591535b91b3d52e6235bd1de9ab2d
Marcos Adriano <mdrick19@gmail.com>
2023-02-21 00:04:26 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usercases/add-account/db-add-account.spec.ts
+++ src/data/usercases/add-account/db-add-account.spec.ts
@@ -91,4 +91,23 @@ describe("DbAddAccount Usecase", () => {
       password: "encrypted_password",
     });
   });
+
+  test("Should throw if AddAccountRepository throws", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(addAccountRepositoryStub, "add")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+
+    const accountPromise = sut.add(accountData);
+    await expect(accountPromise).rejects.toThrow();
+  });
 });
